c4c58d83d323ee2c4d41f5ef58822370a3fffcd6
rabindrapatra <rabindrapatra@corp.com>
2023-01-26 11:28:28 +0530

    using props with typescript react

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,14 +1,16 @@
-type PersonProps={
-   name: string 
-   address: Address
-}
-type Address={
-    city: string 
-    pincode: number
-}
-export const PersonComponent=(props:PersonProps)=>{
-    return <>
-    <br/>
-    {JSON.stringify(props)}
+type PersonProps = {
+  name: string;
+  address: Address;
+};
+type Address = {
+  city: string;
+  pincode: number;
+};
+export const PersonComponent = (props: PersonProps) => {
+  return (
+    <>
+      <br />
+      {JSON.stringify(props)}
     </>
-}
+  );
+};
--- src/components/PersonList.tsx
+++ src/components/PersonList.tsx
@@ -1,30 +1,34 @@
-type PersonData={
-    name: string 
-    age: number
-}
+type PersonData = {
+  name: string;
+  age: number;
+};
 
-type PersonsData={
-    elem:PersonData[]
-}
+type PersonsData = {
+  elem: PersonData[];
+};
 
-
-export const PersonList=(p:PersonsData)=>{
-    return <>
-    {
-        p.elem.map(e=>{
-           return <h2 key={e.name}> {e.name} {e.age}</h2> 
-        })
-    }
+export const PersonList = (p: PersonsData) => {
+  return (
+    <>
+      {p.elem.map((e) => {
+        return (
+          <h2 key={e.name}>
+            {" "}
+            {e.name} {e.age}
+          </h2>
+        );
+      })}
     </>
-}
+  );
+};
 
-export const getPersonsList=()=>{
-    let data:PersonsData={
-        elem:[
-            {name:'Name2', age:20},
-            {name:'Name1', age:10},
-        ]
-    };
+export const getPersonsList = () => {
+  let data: PersonsData = {
+    elem: [
+      { name: "Name2", age: 20 },
+      { name: "Name1", age: 10 },
+    ],
+  };
 
-   return data;
-}
+  return data;
+};
